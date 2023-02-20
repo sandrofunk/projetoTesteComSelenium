d84619edf163a9c804ad216530466fdc4194bc96
sandrofunk <sagfunk@gmail.com>
2023-02-20 14:38:48 -0300

    Pesquisa Google

--- src/main/java/Page/AcessarGooglePage.java
+++ src/main/java/Page/AcessarGooglePage.java
@@ -1,2 +1,18 @@
-package Page;public class AcessarGooglePage {
+package Page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class AcessarGooglePage {
+
+    WebDriver driver;
+
+    public AcessarGooglePage(WebDriver driver) {
+
+        this.driver = driver;
+
+        PageFactory.initElements(driver, this);
+
+    }
+
 }
--- src/test/java/PageTest/AcessarGooglePageTest.java
+++ src/test/java/PageTest/AcessarGooglePageTest.java
@@ -1,2 +1,51 @@
-package PageTest;public class AcessarGooglePageTest {
+package PageTest;
+
+import Page.AcessarGooglePage;
+import com.aventstack.extentreports.Status;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class AcessarGooglePageTest {
+
+    public WebDriver driver;
+    public AcessarGooglePage acessarGooglePage;
+
+    @BeforeClass
+    public void checkCurrentOS() {
+
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
+        }
+    }
+
+    @BeforeMethod
+    public void testCaseSetup() {
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+
+        this.driver.get("https://www.google.com.br");
+        this.driver.manage().window().maximize();
+
+        System.out.println("A página acessada é: " + driver.getCurrentUrl());
+
+    }
+
+    @Test(priority = 1)
+    public void acessandoGoogle() {
+
+        acessarGooglePage = new AcessarGooglePage(driver);
+
+    }
+
 }
